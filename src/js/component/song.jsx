4d83c982be1d895aa56c1d8a8ae95f2a5fe7dd08
crduque <crduque@hotmail.com>
2020-10-26 18:08:13 +0000

    added fetch function that returns an array of urls

--- src/js/component/song.jsx
+++ src/js/component/song.jsx
@@ -1,23 +1,11 @@
 import React, { useState, useEffect } from "react";
 
 export const Songs = props => {
-	const files = [
-		{
-			name: "Cancion 1",
-			link:
-				"https://assets.breatheco.de/apis/sound/files/mario/songs/castle.mp3"
-		},
-		{
-			name: "Cancion 2",
-			link:
-				"https://assets.breatheco.de/apis/sound/files/mario/songs/hurry-starman.mp3"
-		}
-	];
-	let [url, setUrl] = useState("");
+	let [finalUrl, setFinalUrl] = useState("");
 
 	async function myFetch() {
 		let response = await fetch(
-			"https://assets.breatheco.de/apis/sound/all"
+			"https://assets.breatheco.de/apis/sound/songs"
 		);
 		if (!response.ok) {
 			throw new Error(response.status);
@@ -25,60 +13,59 @@ export const Songs = props => {
 			return await response.json();
 		}
 	}
+	let myArray = [];
 	myFetch()
 		.then(songsArray => {
-			// useEffect(
-			// 	() => {
-			// 		songsArray.map((file, index) => {
-			// 			setUrl(file.url);
-			// 		});
-			// 	},
-			// 	[url]
-			// );
-			console.log(songsArray);
+			let myUrl = "https://assets.breatheco.de/apis/sound/";
+			let myCompleteUrl = "";
+			for (let index = 0; index < songsArray.length; index++) {
+				myCompleteUrl = myUrl.concat(songsArray[index].url); //concatena el principio de la url con el final, que es cada elemento del array que devuelve fetch en la propiedad url de cada objeto
+				myArray.push(myCompleteUrl);
+			}
 		})
 		.catch(error => {
 			console.log("Something went wrong", error);
 		});
+	console.log(myArray); // tenemos un array de todos los url
 
-	return (
-		<div>
-			<audio autoPlay controls id="myAudio">
-				<source src={url} type="audio/mpeg" />
-			</audio>
-			<ol>
-				{files.map((file, index) => {
-					return (
-						<li
-							key={index}
-							onClick={() => {
-								setUrl(file.link);
-								console.log(url);
-							}}>
-							{file.name}
-						</li>
-					);
-				})}
-			</ol>
+	// return (
+	// 	<div>
+	// 		<audio autoPlay controls id="myAudio">
+	// 			<source src={finalUrl} type="audio/mpeg" />
+	// 		</audio>
+	// 		<ol>
+	// 			{myArray.map((file, index) => {
+	// 				return (
+	// 					<li
+	// 						key={index}
+	// 						onClick={() => {
+	// 							setFinalUrl(file);
+	// 							console.log(finalUrl);
+	// 						}}>
+	// 						Cancion
+	// 					</li>
+	// 				);
+	// 			})}
+	// 		</ol>
 
-			{/* <button
-				onClick={() => {
-					myAudio.play();
-				}}>
-				Play
-			</button>
-			<button
-				onClick={() => {
-					myAudio.pause();
-				}}>
-				Pause
-			</button> */}
-			{/* <button
-				onClick={() => {
-					setSongPlayingsong(playing => songPlaying + 1);
-				}}>
-				Pause
-			</button> */}
-		</div>
-	);
+	// 		{/* <button
+	// 			onClick={() => {
+	// 				myAudio.play();
+	// 			}}>
+	// 			Play
+	// 		</button>
+	// 		<button
+	// 			onClick={() => {
+	// 				myAudio.pause();
+	// 			}}>
+	// 			Pause
+	// 		</button> */}
+	// 		{/* <button
+	// 			onClick={() => {
+	// 				setSongPlayingsong(playing => songPlaying + 1);
+	// 			}}>
+	// 			Pause
+	// 		</button> */}
+	// 	</div>
+	// );
 };
